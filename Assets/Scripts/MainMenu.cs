0d8898b265ce85beac102b28aa67c8ca297b5bf1
Manu TEAO <mrgeymeurt@gmail.com>
2025-03-02 19:31:54 +0100

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Zone1");
+        SceneManager.LoadSceneAsync("ZoneDev");
     }
 
     public void QuitGame()
