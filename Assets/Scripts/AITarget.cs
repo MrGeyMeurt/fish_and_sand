bf5705eaa4b8fb33ff94353d64a447300c34e093
Manu TEAO <mrgeymeurt@gmail.com>
2025-03-29 00:49:42 +0100

    ✨ Add cooldown and player freeze mechanism

--- Assets/Scripts/AITarget.cs
+++ Assets/Scripts/AITarget.cs
@@ -1,33 +1,68 @@
 using UnityEngine;
 using UnityEngine.AI;
+using StarterAssets;
+using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class AITarget : MonoBehaviour
 {
+    [Header("AI Settings")]
     [SerializeField] private NavMeshAgent m_Agent;
+    [SerializeField] private Animator m_Animator;
+    [Header("Player Settings")]
     [SerializeField] private Transform Target;
-    [SerializeField] private float AttackDistance;
-    private float m_Distance;
+    [SerializeField] private float ColliderDistance;
 
+    [Header("Settings")]
+    [SerializeField] private float CooldownDuration = 3f;
+
+    private ThirdPersonController thirdPersonController;
+    private float m_Distance;
+    private bool isCooldown;
+
+    private float targetMoveSpeed;
+    private float targetSprintSpeed;
     void Start()
     {
-        Debug.Log(Target.position);
-        Debug.Log(m_Agent.transform.position);
+        m_Animator = GetComponent<Animator>();
+        thirdPersonController = Target.GetComponent<ThirdPersonController>(); // Access the ThirdPersonController component on the Target
+
+        targetMoveSpeed = thirdPersonController.MoveSpeed;
+        targetSprintSpeed = thirdPersonController.SprintSpeed;
     }
 
     void Update()
     {
         m_Distance = Vector3.Distance(m_Agent.transform.position, Target.position);
         
-        if (m_Distance < AttackDistance)
+        if (m_Distance < ColliderDistance)
         {
-            Debug.Log("Target reached!");
-            m_Agent.isStopped = true;
+            thirdPersonController.MoveSpeed = 0f;
+            thirdPersonController.SprintSpeed = 0f;
+
+            if(!isCooldown)
+            {
+                thirdPersonController.MoveSpeed = targetMoveSpeed;
+                thirdPersonController.SprintSpeed = targetSprintSpeed;
+            }
+            
+            FindObjectOfType<GameRule>().RemoveLvl();
+            StartCoroutine(Cooldown());
         }
         else
         {
-            m_Agent.isStopped = false;
             m_Agent.destination = Target.position;
         }
+    }
+
+    private IEnumerator Cooldown()
+    {
+        isCooldown = true;
+        Debug.Log("Cooldown started");
+        
+        yield return new WaitForSeconds(CooldownDuration);
+        
+        Debug.Log("Cooldown finished");
+        isCooldown = false;
     }
 }
