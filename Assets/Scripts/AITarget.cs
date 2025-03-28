a2034d2e0a53ccc661c66fc21de5e7a19d16f13a
Manu TEAO <mrgeymeurt@gmail.com>
2025-03-28 18:50:24 +0100

    🎉 Add AITarget script for AI navigation and targeting behavior

---
+++ Assets/Scripts/AITarget.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AITarget : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent m_Agent;
+    [SerializeField] private Transform Target;
+    [SerializeField] private float AttackDistance;
+    private float m_Distance;
+
+    [Header("Navigation Settings")]
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float acceleration = 8f;
+
+    void Start()
+    {
+        Debug.Log(Target.position);
+        Debug.Log(m_Agent.transform.position);
+        m_Agent.speed = moveSpeed;
+        m_Agent.acceleration = acceleration;
+    }
+
+    void Update()
+    {
+        m_Distance = Vector3.Distance(m_Agent.transform.position, Target.position);
+        
+        if (m_Distance < AttackDistance)
+        {
+            Debug.Log("Target reached!");
+            m_Agent.isStopped = true;
+        }
+        else
+        {
+            m_Agent.isStopped = false;
+            m_Agent.destination = Target.position;
+            Debug.Log("Target locked");
+        }
+    }
+}
