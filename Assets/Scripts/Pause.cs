fce277ebbd1d5e34f5ee280e4512c3414d1b6a5e
Manu TEAO <mrgeymeurt@gmail.com>
2025-04-04 23:42:57 +0200

    🎉 Implement pause functionality with input handling and UI integration

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using UnityEngine.InputSystem;
+
+public class Pause : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    private CharacterController _controller;
+    [SerializeField] private GameRule gameRule;
+
+    void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _input = GetComponent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        if (_input.pause)
+        {
+            _input.pause = false;
+            gameRule?.GamePause();
+        }
+    }
+}
