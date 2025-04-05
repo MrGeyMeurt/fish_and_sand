f3939477c3388a939d101ac5db28dcaf5f6d8545
Manu TEAO <mrgeymeurt@gmail.com>
2025-04-05 14:16:52 +0200

    ♻️ Refactor playerName property to use PlayerPrefs for persistent storage

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public static PlayerStats Instance { get; private set; }
-
-    public string playerName = "Player";
+    public string playerName
+    {
+        get => PlayerPrefs.GetString("PlayerName", "Player");
+        set => PlayerPrefs.SetString("PlayerName", value);
+    }
     public float playTime;
     public int score;
     public int hitsTaken;
@@ -22,6 +25,8 @@ public class PlayerStats : MonoBehaviour
         {
             Instance = this;
         }
+        DontDestroyOnLoad(gameObject);
+        Debug.Log($"Nom du joueur chargé : {PlayerStats.Instance.playerName}");
     }
 
     private void Update()
@@ -32,8 +37,34 @@ public class PlayerStats : MonoBehaviour
         }
     }
 
-    public void AddScore(int amount) => score += amount;
-    public void AddHit() => hitsTaken++;
-    public void AddDash() => dashCount++;
+    public void SavePlayerName(string newName)
+    {
+        playerName = newName;
+        PlayerPrefs.Save();
+    }
+
+    public void AddScore(int amount)
+    {
+        if (GameRule.Instance != null && !GameRule.Instance.IsGameOver())
+        {
+            score += amount;
+        }
+    }
+
+    public void AddHit()
+    {
+        if (GameRule.Instance != null && !GameRule.Instance.IsGameOver())
+        {
+            hitsTaken++;
+        }
+    }
+
+    public void AddDash()
+    {
+        if (GameRule.Instance != null && !GameRule.Instance.IsGameOver())
+        {
+            dashCount++;
+        }
+    }
     public void StopCountingTime() => isCountingTime = false;
 }
